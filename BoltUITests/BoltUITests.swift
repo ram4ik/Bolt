12c1e61c72778d30401d5d2a9d4a740529928255
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-15 11:49:06 +0300

    some tests

--- BoltUITests/BoltUITests.swift
+++ BoltUITests/BoltUITests.swift
@@ -10,34 +10,46 @@ import XCTest
 
 class BoltUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let boltApp = XCUIApplication(bundleIdentifier: "ee.mtakso.client")
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    override func setUp() {
+        boltApp.launch()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        boltApp.terminate()
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testBoltAppMenuButton() throws {
+        
+        boltApp.description
+        boltApp.buttons["NavigationView.menuButton"].tap()
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testBoltAppSccoterButton() throws {
+        
+        boltApp.buttons["NavigationView.scooterButton"].tap()
+        boltApp.buttons["unlock.scooter.button"].tap()
+        
+        // turn on flash light
+        boltApp.buttons["UnlockScooter.flashButton"].tap()
+        // turn off flash light
+        boltApp.buttons["UnlockScooter.flashButton"].tap()
+    }
+    
+    func testBoltAppFoodButton() throws {
+        
+        boltApp.buttons["NavigationView.foodButton"].tap()
+    }
+    
+    func testBoltAppWhereToAddressButton() throws {
+        
+        boltApp.buttons["WhereTo.addressBar"].tap()
+    }
+    
+    func testBoltAppNavigationButtons() throws {
+        
+        boltApp.buttons["nav.back.button"].tap()
+        boltApp.buttons["nav.right.button"].tap()
     }
 }
